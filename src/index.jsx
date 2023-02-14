4133c3409b49bfa9866a195da55f4dcd68c19bb5
SohanGnd <sohan.gonord@hotmail.com>
2023-02-14 18:49:47 +0100

    Modification lié à l'ajout du fichier routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,41 +1,16 @@
 // React Framework //
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ReactDOM from 'react-dom'
-
-// Pages by React BrowserRouter //
-import About from './pages/About'
-import Home from './pages/Home'
-import Singleproduct from './pages/Singleproduct'
-
-// Components Header & Footer & Error & Products //
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Error from './components/Error'
 
 // CSS //
 import './utils/style/main.css'
 
+import App from './App'
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/products/:productId">
-          <Singleproduct />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route>
-          <Error />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <div className="contenu">
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 )
