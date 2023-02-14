ca479c6d78376a39a5c32e47c789198bc321510a
SohanGnd <sohan.gonord@hotmail.com>
2023-02-14 18:48:12 +0100

    Ajout du fichier App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './utils/style/main.css'
+import RoutesPath from '../src/routes/RoutesPath'
+
+function App() {
+  return (
+    <div className="App">
+      <RoutesPath />
+    </div>
+  )
+}
+
+export default App
