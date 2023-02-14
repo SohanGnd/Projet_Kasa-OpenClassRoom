c6531cbae58bceee9d41afbe0dfc6dab14760ac8
SohanGnd <sohan.gonord@hotmail.com>
2023-02-14 18:37:18 +0100

    Ajout du fichier routes

---
+++ src/routes/RoutesPath.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+// Pages by React BrowserRouter //
+import About from '../pages/About'
+import Home from '../pages/Home'
+import Singleproduct from '../pages/Singleproduct'
+
+// Components Header & Footer & Error & Products //
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Error from '../components/Error'
+
+function RoutesPath() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:productId">
+          <Singleproduct />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
+}
+
+export default RoutesPath
