8e800bb2f9cac1071b843d32fedf2bfacaddaa31
SohanGnd <sohan.gonord@hotmail.com>
2023-03-06 15:12:48 +0100

    Ajout de State aux Collapse

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Arrow from '../../assets/arrow.png'
 
 const Collapse = ({ title, content }) => {
+  const [Open, setOpen] = useState(false)
+
   const isCollapse = (e) => {
     e.preventDefault()
-    const divText = e.target.nextSibling
-    const arrow = e.target.lastChild
-
-    if (!divText.classList.contains('show')) {
-      divText.classList.add('show')
-      arrow.classList.add('rotate')
-    } else {
-      divText.classList.remove('show')
-      arrow.classList.remove('rotate')
-    }
+    setOpen(!Open)
   }
+
   return (
     <div className="collapse collapse_about">
       <button type="button" className="collapse_button" onClick={isCollapse}>
         {title}
 
-        <img src={Arrow} alt="" className="collapse_arrow" />
+        <img
+          src={Arrow}
+          alt=""
+          className={'collapse_arrow ' + (Open ? 'rotate' : '')}
+        />
       </button>
-      <div className="collapse_content">
+      <div className={'collapse_content ' + (Open ? 'show' : '')}>
         {Array.isArray(content) ? (
           <ul className="collapse_list">
             {content.map((equipment, index) => (
